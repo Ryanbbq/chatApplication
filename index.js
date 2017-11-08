e10f83572fa3d55c170d785415c2f74c79496650
ryanlb22 <rlb.lebon@gmail.com>
2017-11-08 05:54:08 +0000

    beautified text shift-cntrl-b

--- index.js
+++ index.js
@@ -7,26 +7,36 @@ app.get('/', function(req, res){
   res.sendFile('index.html', { root: __dirname });
 });
 
-io.on('connection',function(socket){
-  var addedUser = false;
-  console.log('a user connected');
+var i = 0;
 
+io.on('connection',function(socket){
+  
+  console.log('a user connected');
+  i++;
+  console.log('Users connected:' + i);
+  
     socket.on('chat message', function(data) {
     console.log('message: ' + data.message);
     console.log('name: ' + data.name);
+    data.users = i;
     io.emit('chat message', data);
     });
     
-    
-    
-    
-    socket.on('disconnect',function(){
+   socket.on('disconnect',function(){
         console.log('the user disconnected');
+        i--;
+        console.log('Users connected : ' + i);
     });
+    
+    socket.on('users connected', function(i){
+      io.emit('users connected', i);
+    });
+    
     
     
 });
 
 http.listen(process.env.PORT || 5000, function(){
+console.log('http://chat-application-ryanlb22.c9users.io:8080/');
 console.log("Server started");
 });

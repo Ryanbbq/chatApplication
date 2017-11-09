90e7aefb8333ac6037ed08ebefff552dfa40b736
Kristine Laranjo <klaranjo@csumb.edu>
2017-11-09 08:13:29 +0000

    formatted code

--- index.js
+++ index.js
@@ -2,39 +2,36 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
-
 app.get('/', function(req, res){
   res.sendFile('index.html', { root: __dirname });
 });
 
 var i = 0;
 
-io.on('connection',function(socket){
+io.on('connection', function(socket) {
   
   console.log('a user connected');
   i++;
   console.log('Users connected:' + i);
   
-    socket.on('chat message', function(data) {
-      console.log('message: ' + data.message);
-      console.log('name: ' + data.name);
-      io.emit('chat message', data);
-    });
+  socket.on('chat message', function(data) {
+    console.log('message: ' + data.message);
+    console.log('name: ' + data.name);
+    io.emit('chat message', data);
+  });
     
-   socket.on('disconnect',function(){
-        console.log('the user disconnected');
-        i--;
-        console.log('Users connected : ' + i);
-    });
+  socket.on('disconnect', function() {
+    console.log('the user disconnected');
+    i--;
+    console.log('Users connected : ' + i);
+  });
     
-    socket.on('users connected', function(){
-      io.emit('users connected', i);
-    });
-    
-    
+  socket.on('users connected', function() {
+    io.emit('users connected', i);
+  });
 });
 
-http.listen(process.env.PORT || 5000, function(){
-console.log('http://chat-application-ryanlb22.c9users.io:8080/');
-console.log("Server started");
+http.listen(process.env.PORT || 5000, function() {
+  console.log('http://chat-application-ryanlb22.c9users.io:8080/');
+  console.log("Server started");
 });
